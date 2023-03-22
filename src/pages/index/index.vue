529e1c7046c1fb36c42178fa61c2e28a4d8cdb9c
waset <waset@foxmail.com>
2023-03-22 11:25:41 +0800

    chore(deps): use unplugin-auto-import

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,8 +8,8 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+const title = $ref('Hello')
+console.log(title)
 </script>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    uni({
+      vueOptions: {
+        reactivityTransform: true
+      }
+    }),
+    AutoImport({
+      dts: 'runtime/auto-import.d.ts',
+      imports: [
+        'vue',
+        'vue/macros',
+      ],
+    }),
+  ],
 });

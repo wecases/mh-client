45070d0da8024d0452c2968b561ad653a8baad7d
waset <waset@foxmail.com>
2023-03-24 18:30:58 +0800

    refactor(page): 去掉导航错误样式

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,11 @@ onHide(() => {
 	console.log("App Hide");
 });
 </script>
-<style>
+<style lang="scss">
 @font-face {
 	font-family: 'testFont';
 	src: url('./static/zyytBold.ttf');
 }
-
-/*每个页面公共css */
 
 page::after {
 	content: "";
@@ -29,10 +27,5 @@ page::after {
 	bottom: 0;
 	z-index: -2;
 	background: #f1f1f1;
-}
-
-/* 底部导航栏 */
-/deep/ .uni-tabbar-bottom .uni-tabbar {
-	padding-top: 0.3rem;
 }
 </style>

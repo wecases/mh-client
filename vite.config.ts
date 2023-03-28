fb2b583154a6df815d1288693dfe959301e5b151
waset <waset@foxmail.com>
2023-03-28 18:45:05 +0800

    build(develop): 生产环境时移除 console

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,16 @@ import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        // 生产环境时移除console
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
   plugins: [
     Uni({
       vueOptions: {
@@ -34,7 +44,7 @@ export default defineConfig({
         'src/stores',
         'src/utils',
       ],
-      vueTemplate:true
+      vueTemplate: true
     }),
 
     // https://github.com/dishait/vite-plugin-use-modules
